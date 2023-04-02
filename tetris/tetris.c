8735f4e3112a2ada75fc597d07c23918b28fa0cb
David Latham <david.latham.nz@gmail.com>
2023-04-02 18:48:50 +1200

    interim

--- tetris/tetris.c
+++ tetris/tetris.c
@@ -456,7 +456,7 @@ bool menu() {
     vdp_setCursor2(13-(4/2),20);
     vdp_print("DAVE");
     vdp_setCursor2(13-(4/2),21);
-    vdp_print("V2.2");
+    vdp_print("V3.0");
 
     while (true) {
         if (getJoyStatus(0) & Joy_Button) {
