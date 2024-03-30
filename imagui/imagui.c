ae5df187a5c617106e98e1489a1b58fb1b1637ed
David Latham <david.latham.nz@gmail.com>
2024-03-31 00:20:05 +1300

    working

--- imagui/imagui.c
+++ imagui/imagui.c
@@ -8,13 +8,71 @@
 #include "NABU-LIB.h"
 #include "RetroNET-IAControl.h"
 
-#include "NabuTracker.h"
-#include "nabu-games.h"
+//#include "NabuTracker.h"
 #include "imagui.h"
 
 char namebuf[64];
 char textbuf[32];
 
+void vdp_setPatternColor(uint8_t color) {
+    vdp_setWriteAddress(_vdpColorTableAddr);
+    for (uint16_t i = 0; i < 0x1800; i++) {
+        IO_VDPDATA = color;
+        nop();
+        nop();
+        nop();
+        nop();
+    }
+}
+
+void printAtLocationBuf(uint8_t x, uint8_t y, uint8_t *text) {
+    uint16_t offset = y * _vdpCursorMaxXFull + x;
+    uint8_t *start = text;
+
+    while (*start != 0x00) {
+
+      _vdp_textBuffer[offset] = *start;
+      offset++;
+      start++;
+    }
+}
+
+
+void delay(uint8_t frames) {
+    while(frames > 0) {
+        vdp_waitVDPReadyInt();
+        frames --;
+    }
+}
+
+void hardReset(void) {
+    __asm
+        // Disable interrupts
+        // di
+        ld a,0xf3;
+        ld (0x8880),a;
+
+        // Enable rom shadow
+        // lda a,0x02
+        ld a,0x3e;
+        ld (0x8881),a;
+        ld a,0x02;
+        ld (0x8882),a;
+
+        // out(0x00),a
+        ld a,0xd3;
+        ld (0x8883),a;
+        ld a,0x00;
+        ld (0x8884),a;
+
+        // restart at 0x00
+        // rst 0x00
+        ld a,0xc7;
+        ld (0x8885),a;
+
+        jp 0x8880;
+    __endasm;
+}
 
 /*
  * Lifted from DJs code drop in discord.
@@ -22,15 +80,9 @@ char textbuf[32];
  * by inserting a 0x00 into position 29 if we get there.
  */
 void toupper(uint8_t *text) {
-    uint8_t p = 0;
     for (uint16_t i = 0; text[i] != 0x00; i++) {
         if (text[i] >= 'a')
             text[i] = text[i] - 32;
-        p ++;
-        if (p==29) {
-            text[i] = 0x00;
-            return;
-        }
     }
 }
 
@@ -46,7 +98,7 @@ void init(void) {
     initNABULib();
     vdp_clearVRAM();
     vdp_initG2Mode(1, false, false, false, false);
-    vdp_loadPatternTable(FAT,768);
+    vdp_loadPatternTable(ASCII,1024);
     uint16_t _vdpColorTableAddr = 0x2000;
     uint16_t _vdpColorTableSize = 0x1800;
     vdp_setWriteAddress(_vdpColorTableAddr);
@@ -64,98 +116,36 @@ void draw_menu(int **arr, uint8_t size) {
     uint8_t menu_x = 0;
 
     for (uint8_t i = 0; i<size; i++) {
-        printAtLocationBuf(menu_x, 0, arr[i]);
+        printAtLocationBuf(menu_x + 1, 0, arr[i]);
         menu_x = menu_x + str_len(arr[i]) + 1;
     }
-    vdp_waitVDPReadyInt();
-    vdp_refreshViewPort();
 }
 
+void underline_menu_item( uint8_t item_num, int**arr) {
+    uint8_t menu_x = 1;
+    uint8_t menu_y = 1;
 
-/*
- * Display parents screen
- */
-uint8_t parents_screen(uint8_t p_count) {
-    vdp_clearScreen();
-    draw_menu(main_menu, MAIN_MENU_LEN);
-    printAtLocationBuf(1, 2, "MAIN MENU");
-    for (uint8_t i = 0; i< p_count; i++) {
-        ia_getParentName(i, namebuf);
-        toupper(namebuf);
-        sprintf(textbuf, "%d %s", i, namebuf);
-        printAtLocationBuf(1, 6+i, textbuf);
+    for (uint8_t i = 0; i<item_num; i++) {
+        menu_x = menu_x + str_len(arr[i]) + 1;
     }
-    vdp_waitVDPReadyInt();
-    vdp_refreshViewPort(); 
-
-    uint8_t p_id = 0xFF;
-    while (p_id == 0xFF) {
-        uint8_t _key = getChar();
-        if ( _key == 0x1b) return 254;
-
-        if ( _key >= 0x30 || _key < p_count + 0x30 ) {
-            p_id = _key - 0x30;
-        }
-        vdp_waitVDPReadyInt();
+    for (uint8_t i = 0; i<str_len(arr[item_num]); i++) {
+        printAtLocationBuf(menu_x + i, menu_y, "~");
     }
-    return p_id;
-}
-
-/*
- * Display children screen
- */
-uint8_t children_screen(uint8_t p_id) {
-    vdp_clearScreen();
-    draw_menu(main_menu, MAIN_MENU_LEN);
-
-    ia_getParentName(p_id, namebuf);
-    toupper(namebuf);
-
-    printAtLocationBuf(1, 2, namebuf);
-
-    uint8_t c_count = ia_getChildCount(p_id);
-
-    for (uint8_t i = 0; i < c_count; i++) {
-        ia_getChildName(p_id, i, namebuf);
-        toupper(namebuf);
-        sprintf(textbuf, "%d %s", i, namebuf);
-        printAtLocationBuf(1, 6+i, textbuf);
-    }
-
-    vdp_waitVDPReadyInt();
-    vdp_refreshViewPort();
-
-    uint8_t c_id = 255;
-    while (c_id == 255) {
-        uint8_t _key = getChar();
-        if ( _key >= 0x30 || _key < c_count + 0x30 ) {
-            c_id = _key - 0x30;
-        }
-        vdp_waitVDPReadyInt();
-    }
-    return c_id;
 }
 
 void main(void) {
     init();
-    draw_menu(main_menu, 4);
-    // Get channel data
-    uint8_t p_count = ia_getParentCount();
+    draw_menu(main_menu, MAIN_MENU_LEN);
+    underline_menu_item(2, main_menu);
 
-    while (1) {
-        uint8_t p_id = parents_screen(p_count);
-        if (p_id == 254) break;
-        uint8_t c_id = children_screen(p_id);
+    vdp_waitVDPReadyInt();
+    vdp_refreshViewPort();
 
-        ia_getChildName(p_id, c_id, namebuf);
-        toupper(namebuf);
-        sprintf(textbuf, "--> %s", namebuf);
-        printAtLocationBuf(2, 23, textbuf);
-
-        ia_setSelection(p_id, c_id);
-        vdp_waitVDPReadyInt();
-        vdp_refreshViewPort();
-        delay(2000);
-        //hardReset();
+    while (true) {
+        if (isKeyPressed()) {
+            if (getChar() == 0x1b) {
+                break;
+            }
+        }
     }
 }
